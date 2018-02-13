74f3a6ec3b0502da5cc080e10a0a11768068bf03
YixuanLi1226 <2305021l@student.gla.ac.uk>
2018-02-13 13:25:16 +0000

    first commit

---
+++ populate_rango.py
@@ -0,0 +1,85 @@
+import os
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tango_with_django_project.settings')
+
+import django
+django.setup()
+from rango.models import Category, Page
+
+def populate():
+    # First, we will create lists od dictionaries containing the pages
+    # we want to add into category.
+    # Then we will create a dictionary of dictionaries for our categories
+    # This might seem a little bit confusing, but it allows us to iterate
+    # through each data structure, and add the data to our models.
+
+    python_pages = [
+        {"title": "Official Python Tutorial",
+         "url": "http://docs.python.org/2/tutorial/",
+         "views": 20},
+        {"title": "How to Think like a Computer Scientise",
+         "url": "http://www.greenteapress.com/thinkpython/",
+         "views": 35},
+        {"title": "Learn Python in 10 Minutes",
+         "url": "http://www.korokithakis.net/tutorials/python/",
+         "views": 40}]
+
+    django_pages = [
+        {"title": "Official Python Tutorial",
+         "url": "http://docs.djangoproject.com/en/1.9/intro/tutorial01/",
+         "views": 10},
+        {"title": "Django Rocks",
+         "url": "http://www.djangorocks.com/",
+         "views": 65},
+        {"title": "How to tango with django",
+         "url": "http://www.tangowithdjango.com/",
+         "views": 55}]
+
+    other_pages = [
+        {"title": "Bottle",
+         "url": "http://bottlepy.org/docs/dev/",
+         "views": 35},
+        {"title": "Flask",
+         "url": "http://flask.pocoo.org",
+         "views": 5}]
+
+    cats = {"Python": {"pages": python_pages,"views": 128, "likes": 64},
+            "Django": {"pages": django_pages,"views": 64, "likes": 32},
+            "Other Frameworks": {"pages": other_pages,"views": 32, "likes": 16}}
+
+    # add them to the dictionary above.
+
+    # The code below goes through the cats dictionary, then adds each category,
+    # and then adds all the associated pages for that category.
+    # if you are using Python 2.x then use cats.iteritems() see
+    # https://docs.quantifiedcode.com/python-anti-patterns/readability/
+    # for more information about how to iterate over a dictionary properly.
+
+    for cat, cat_data in cats.items():
+        c = add_cat(cat, cat_data["views"], cat_data["likes"])
+        for p in cat_data["pages"]:
+            add_page(c, p["title"], p["url"], p["views"])
+
+    # Print out the categories we have added.
+    for c in Category.objects.all():
+        for p in Page.objects.filter(category=c):
+            print("- {0} - {1}".format(str(c), str(p)))
+
+def add_page(cat, title, url, views):
+    p = Page.objects.get_or_create(category=cat, title=title)[0]
+    p.url = url
+    p.views = views
+    p.save()
+    return p
+
+def add_cat(name, views, likes):
+    c = Category.objects.get_or_create(name=name)[0]
+    c.views = views
+    c.likes = likes
+    c.save()
+    return c
+
+# Start execution here!
+
+if __name__ == '__main__':
+    print("Starting Rango population script...")
+    populate()
---
+++ rango/views.py
@@ -0,0 +1,174 @@
+from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.core.urlresolvers import reverse
+from rango.models import Category
+from rango.models import Page, UserProfile
+from rango.forms import CategoryForm
+from rango.forms import PageForm
+from rango.forms import UserForm, UserProfileForm
+from datetime import datetime
+
+def add_category(request):
+	form = CategoryForm()
+
+	# A HTTP POST?
+	if request.method == 'POST':
+		form = CategoryForm(request.POST)
+
+		if form.is_valid():
+			form.save(commit=True)
+			return index(request)
+
+		else:
+			print(form.errors)
+
+	return render(request, 'rango/add_category.html', {'form': form})
+
+
+def add_page(request, category_name_slug):
+	try:
+		category = Category.objects.get(slug=category_name_slug)
+	except Category.DoesNotExist:
+		category = None
+
+	form = PageForm()
+	if request.method == 'POST':
+		form = PageForm(request.POST)
+		if form.is_valid():
+			if category:
+				page = form.save(commit=False)
+				page.category = category
+				page.views = 0
+				page.save()
+				return show_category(request, category_name_slug)
+			else:
+				print(form.errors)
+	context_dict = {'form': form, 'category': category}
+	return render(request, 'rango/add_page.html', context_dict)
+
+def show_category(request, category_name_slug):
+	context_dic = {}
+
+	try:
+		category = Category.objects.get(slug=category_name_slug)
+		pages = Page.objects.filter(category=category)
+		context_dic['pages'] = pages
+		context_dic['category'] = category
+	except Category.DoesNotExist:
+		context_dic['pages'] = None
+		context_dic['category'] = None
+
+	return render(request, 'rango/category.html', context_dic)
+
+def index(request):
+
+    #return HttpResponse("Rango says hey there partner!<br/><a href='/rango/about/'>About</a>")
+	
+	#Construct a dictionary to pass to the template engin as its context
+	#Note the key boldmessage is the same as {{boldmessage}} in the template!
+    request.session.set_test_cookie()
+    category_list = Category.objects.order_by('-likes')[:5]
+    page_list = Page.objects.order_by('-views')[:5]
+    context_dict = {'categories' : category_list, 'pages': page_list}
+
+
+
+    visitor_cookie_handler(request)
+    context_dict['visits'] = request.session['visits']
+    # Return a rendered response to send to the client
+    # We make use of the shortcut function to make our lives easier
+	# Note that the first parameter is the template we wish to use
+    response = render(request, 'rango/index.html', context=context_dict)
+    return response
+def about(request):
+    if request.session.test_cookie_worked():
+        print("TEST COOKIE WORKED!")
+        request.session.delete_test_cookie()
+    #return HttpResponse("Rango says here is the about page.<br/><a href='/rango/about/'>Index</a>")
+    context_dict = {}
+    visitor_cookie_handler(request)
+    context_dict['visits'] = request.session['visits']
+    return render(request, 'rango/about.html', context=context_dict)
+
+def register(request):
+	registered = False
+	if request.method == 'POST':
+		user_form = UserForm(data=request.POST)
+		profile_form = UserProfileForm(data=request.POST)
+
+		if user_form.is_valid() and profile_form.is_valid():
+			user = user_form.save()
+			user.set_password(user.password)
+			user.save()
+			profile = profile_form.save(commit=False)
+			profile.user = user
+
+			if 'picture' in request.FILES:
+				profile.picture = request.FILES['picture']
+			profile.save()
+			registered = True
+		else:
+			print(user_form.errors, profile_form.errors)
+	else:
+		user_form = UserForm()
+		profile_form = UserProfileForm()
+
+	return render(request, 'rango/register.html',
+				  {
+					  'user_form': user_form,
+					  'profile_form': profile_form,
+					  'registered': registered
+				  })
+
+def user_login(request):
+	if request.method == 'POST':
+		username = request.POST.get('username')
+		password = request.POST.get('password')
+
+		user = authenticate(username=username, password=password)
+
+		if user:
+			if user.is_active:
+				login(request, user)
+				return HttpResponseRedirect(reverse('index'))
+			else:
+				return HttpResponse("Your Rango account is disabled.")
+		else:
+			print("Invalid login details: {0}, {1}".format(username, password))
+			return HttpResponse("Invalid login details supplied.")
+	else:
+		return render(request, 'rango/login.html', {})
+
+@login_required
+def restricted(request):
+    context_dict = {'boldmessage': ", you can see this text!"}
+    return render(request, 'rango/restricted.html', context_dict)
+
+@login_required
+def user_logout(request):
+	logout(request)
+	return HttpResponseRedirect(reverse('index'))
+
+# A helper method
+def get_server_side_cookie(request, cookie, default_val=None):
+    val = request.session.get(cookie)
+    if not val:
+        val = default_val
+    return val
+
+def visitor_cookie_handler(request):
+    visits = int(get_server_side_cookie(request, 'visits', '1'))
+
+    last_visit_cookie = get_server_side_cookie(request, 'last_visit', str(datetime.now()))
+    last_visit_time = datetime.strptime(last_visit_cookie[:-7], '%Y-%m-%d %H:%M:%S')
+
+    if(datetime.now() - last_visit_time).days > 0:
+        visits = visits + 1
+        request.session['last_visit'] = str(datetime.now())
+    else:
+        visits = 1
+        request.session['last_visit'] = last_visit_cookie
+
+    request.session['visits'] = visits
